d4c2677fc1a478f60061910388f1fc8715a069c7
Dharshana Warusavithrana <dharshana.warusavitharana@assurity.co.nz>
2020-08-06 10:21:51 +1200

    Adding report portal IO

--- src/test/java/com/assurity/automation/demo/tests/EmployeeServiceTest.java
+++ src/test/java/com/assurity/automation/demo/tests/EmployeeServiceTest.java
@@ -8,12 +8,15 @@ import com.assurity.automation.utilities.rest.GenericRestClient;
 import com.assurity.automation.demo.service.model.Employee;
 import org.springframework.util.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.CustomAttribute;
 import org.testng.annotations.Test;
 
 import java.net.URISyntaxException;
+import java.util.Map;
 
 public class EmployeeServiceTest extends FrameworkBaseTest {
     GenericRestClient genericRestClient;
+
     @BeforeTest
     public void initTest() throws URISyntaxException {
         TestContext context = new TestContext();
@@ -22,41 +25,52 @@ public class EmployeeServiceTest extends FrameworkBaseTest {
     }
 
     @Test
-    public void addEmployeeTestcase()
-    {
+    public void addEmployeeTestcase() {
         Employee employee = new Employee();
         employee.setId(1234);
         employee.setEmail("dkasunw@gmail.com");
         employee.setFirstName("user");
         employee.setLastName("demo");
-        genericRestClient.doPost("/employees/adduser/",employee);
+        genericRestClient.doPost("/employees/adduser/", employee);
         Assert.notNull(getResponse(), "List is empty, Employee not added");
     }
 
     @Test(dataProvider = "assurity.testng.dataprovider", dataProviderClass = TestNgDataProvider.class)
-    public void addEmployeeDataProviderTestcase(Object name, Object id)
-    {
-        System.out.println("++++++++++++++++++++++++++++"+ name);
-        System.out.println("++++++++++++++++++++++++++++"+ id);
+    @CustomAttribute(name = "dataProvider", values = {"assurity.xsls.dataprovider"})
+    public void addEmployeeDataProviderTestcase(Object data) {
+        Map<String, Object> data2 = (Map<String, Object>) data;
+
         Employee employee = new Employee();
-        employee.setId(Integer.valueOf(String.valueOf(id)));
-        employee.setEmail("dkasunw@gmail.com");
-        employee.setFirstName(String.valueOf(name));
+        employee.setId((int) Double.parseDouble(data2.get("id").toString()));
+        employee.setEmail(data2.get("email").toString());
+        employee.setFirstName(data2.get("name").toString());
         employee.setLastName("demo");
-        genericRestClient.doPost("/employees/adduser/",employee);
-
+        Object response = genericRestClient.doPost("/employees/adduser/", employee);
+        Assert.notNull(response, "Empty Response with no return status");
     }
+
+    @Test(dataProvider = "assurity.testng.dataprovider", dataProviderClass = TestNgDataProvider.class)
+    @CustomAttribute(name = "dataProvider", values = {"assurity.mysql.dataprovider"})
+    public void addEmployeeDataProviderTestcase2(Object data) {
+        Map<String, Object> data2 = (Map<String, Object>) data;
+
+        Employee employee = new Employee();
+        employee.setId((int) Double.parseDouble(data2.get("id").toString()));
+        employee.setEmail(data2.get("email").toString());
+        employee.setFirstName(data2.get("name").toString());
+        employee.setLastName("demo");
+        Object response = genericRestClient.doPost("/employees/adduser/", employee);
+        Assert.notNull(response, "Empty Response with no return status");
+    }
+
     @Test
-    public void getEmployeeTestcase()
-    {
-        Object response  = genericRestClient.doGetRequest( "/employees/getuser/");
-        response.toString();
-        Assert.notNull(response, "List is empty, Employee not added");
+    public void getEmployeeTestcase() {
+        Object response = genericRestClient.doGetRequest("/employees/getuser/");
+        Assert.notNull(response, "Empty Response with no return status");
     }
 
-    private Object getResponse()
-    {
-        Object response  = genericRestClient.doGetRequest( "/employees/getuser/");
+    private Object getResponse() {
+        Object response = genericRestClient.doGetRequest("/employees/getuser/");
         return response;
     }
 }
